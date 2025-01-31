af467bf5dd6d390191d7d9c8aa612d6ea59ed34b
Pavel Naydenov <pavel.snk@gmail.com>
2025-01-31 17:04:30 +0300

    Изменен API-key. Сервис перестал работать бесплатно. Проблемы с доступом из России

--- ip_weather/settings.py
+++ ip_weather/settings.py
@@ -1,4 +1,4 @@
-API_key = "f4257bc56b67acd935c7788f87d64045"
+API_key = "b7bb8f489c00ee5e4b453eaaaa42b648"
 
 IP_API_URL = "http://ip-api.com/json/{query}"
 OPENWEATHER_URL = "https://ru.api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={API_key}&lang=ru&units=metric"
